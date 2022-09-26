dbbef45442c8bb3adb213dbe7d91d95d76aff793
mmassie1 <mmassie1@students.kennesaw.edu>
2022-09-26 13:12:02 -0400

    Update calcGUI.py

--- 3 calcGUI/calcGUI.py
+++ 3 calcGUI/calcGUI.py
@@ -1,6 +1,9 @@
+from distutils.cmd import Command
 from tkinter import *
 from math import sqrt as sqr
-
+from math import sin as Sin
+from math import cos as Cos
+from math import tan as Tan
 
 class Calculator(Frame):
     """
@@ -74,6 +77,9 @@ class Calculator(Frame):
         e = e.replace("²", "**2")
         e = e.replace("^", "**")
         e = e.replace("÷", "/")
+        e = e.replace("sin", "Sin")
+        e = e.replace("cos", "Cos")
+        e = e.replace("tan", "Tan")
 
         try:
             ans = eval(e)
@@ -144,6 +150,15 @@ class Calculator(Frame):
         Creates the widgets to be used in the grid.
         :return: None
         """
+        self.sin_bttn = Button(self, text="sin", width=9, height=3, command=lambda: self.add_chr('sin'))
+        self.sin_bttn.grid(row=2, column=6)
+        
+        self.cos_bttn = Button(self, text="cos", width=9, height=3, command=lambda: self.add_chr('cos'))
+        self.cos_bttn.grid(row=3, column=6)
+
+        self.tan_bttn = Button(self, text="tan", width=9, height=3, command=lambda: self.add_chr('tan'))
+        self.tan_bttn.grid(row=4, column=6)
+
         self.eq_bttn = Button(self, text="=", width=20, height=3, bg="lightgrey", command=lambda: self.calculate())
         self.eq_bttn.grid(row=4, column=4, columnspan=2)
 
@@ -186,7 +201,7 @@ class Calculator(Frame):
         self.six_bttn = Button(self, text="6", width=9, height=3, command=lambda: self.add_chr(6))
         self.six_bttn.grid(row=2, column=2)
 
-        self.one_bttn = Button(self, text="1", width=9, height=3, command=lambda: self.add_chr(1))
+        self.one_bttn = Button(self, text="Morgan", width=9, height=3, command=lambda: self.add_chr("Massie"))
         self.one_bttn.grid(row=3, column=0)
 
         self.two_bttn = Button(self, text="2", width=9, height=3, command=lambda: self.add_chr(2))
